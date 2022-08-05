6467ded1ed4dc0c17be6052c876b0ef50f4fa0c8
Jardel Kuhn <jardelkuhn@gmail.com>
2022-08-05 07:15:08 -0700

    first commit

---
+++ src/main/java/br/dev/kuhn/springboottesting/model/Employee.java
@@ -0,0 +1,29 @@
+package br.dev.kuhn.springboottesting.model;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String emailName;
+
+}
---
+++ src/main/java/br/dev/kuhn/springboottesting/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package br.dev.kuhn.springboottesting.repository;
+
+import br.dev.kuhn.springboottesting.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
