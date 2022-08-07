eb07b973fbf4490621334a1ec41042b01f98b366
Jardel Kuhn <jardelkuhn@gmail.com>
2022-08-07 08:46:04 -0700

    service save method test exception implementation

--- src/test/java/br/dev/kuhn/springboottesting/service/EmployeeServiceTests.java
+++ src/test/java/br/dev/kuhn/springboottesting/service/EmployeeServiceTests.java
@@ -1,7 +1,9 @@
 package br.dev.kuhn.springboottesting.service;
 
+import br.dev.kuhn.springboottesting.exception.ResourceNotFoundException;
 import br.dev.kuhn.springboottesting.model.Employee;
 import br.dev.kuhn.springboottesting.repository.EmployeeRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,7 +12,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 import java.util.Optional;
 
@@ -28,6 +33,21 @@ public class EmployeeServiceTests {
     @BeforeEach
     public void setup(){
         employee = Employee.builder().firstName("Jardel").lastName("Kuhn").email("mail@mail.com").build();
+    }
+
+    @Test
+    public void givenExistingEmail_whenSaveEmployeeObject_thenThrowsException () {
+        //g
+        given(employeeRepository.findByEmail(employee.getEmail())).willReturn(Optional.of(employee));
+        //given(employeeRepository.save(employee)).willReturn(employee);
+
+        //w
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            employeeService.saveEmployee(employee);
+        });
+
+        //t
+        verify(employeeRepository, never()).save(any(Employee.class));
     }
 
     @Test
