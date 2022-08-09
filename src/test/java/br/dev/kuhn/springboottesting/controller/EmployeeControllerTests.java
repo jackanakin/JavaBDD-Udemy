2e5b3c389dbb3f0967b657db78bce0d733556c53
Jardel Kuhn <jardelkuhn@gmail.com>
2022-08-09 06:38:10 -0700

    refactored controller to use static import

--- src/test/java/br/dev/kuhn/springboottesting/controller/EmployeeControllerTests.java
+++ src/test/java/br/dev/kuhn/springboottesting/controller/EmployeeControllerTests.java
@@ -3,24 +3,25 @@ package br.dev.kuhn.springboottesting.controller;
 import br.dev.kuhn.springboottesting.model.Employee;
 import br.dev.kuhn.springboottesting.service.EmployeeServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.CoreMatchers.is;
 
 @WebMvcTest
 public class EmployeeControllerTests {
@@ -40,32 +41,32 @@ public class EmployeeControllerTests {
         List<Employee> employeeList = new ArrayList<>();
         employeeList.add(Employee.builder().firstName("Jardel").lastName("Kuhn").email("mail@mail.com").build());
         employeeList.add(Employee.builder().firstName("Jardel2").lastName("Kuhn2").email("mail2@mail.com").build());
-        BDDMockito.given(employeeService.getAllEmployee()).willReturn(employeeList);
+        given(employeeService.getAllEmployee()).willReturn(employeeList);
 
         //w
         ResultActions response = mockMvc.perform(get("/api/employees"));
 
         //t
-        response.andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", CoreMatchers.is(employeeList.size())))
-                .andDo(MockMvcResultHandlers.print());
+        response.andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", is(employeeList.size())))
+                .andDo(print());
     }
 
     @Test
     public void givenEmployeeObject_whenCreateEmployee_thenReturnSavedEmployee() throws Exception {
         Employee employee = Employee.builder().firstName("Jardel").lastName("Kuhn").email("mail@mail.com").build();
-        BDDMockito.given(employeeService.saveEmployee(ArgumentMatchers.any(Employee.class)))
+        given(employeeService.saveEmployee(ArgumentMatchers.any(Employee.class)))
                 .willAnswer((invocationOnMock) -> invocationOnMock.getArgument(0) );
 
         ResultActions response = mockMvc.perform(post("/api/employees")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(employee)));
 
-        response.andExpect(MockMvcResultMatchers.status().isCreated())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName", CoreMatchers.is(employee.getFirstName())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName", CoreMatchers.is(employee.getLastName())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.email", CoreMatchers.is(employee.getEmail())))
-                .andDo(MockMvcResultHandlers.print());
+        response.andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName", is(employee.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(employee.getLastName())))
+                .andExpect(jsonPath("$.email", is(employee.getEmail())))
+                .andDo(print());
     }
 
 }
