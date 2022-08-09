78a9d9e0f79ae46f8803765f44a95fa7925b1072
Jardel Kuhn <jardelkuhn@gmail.com>
2022-08-09 07:11:27 -0700

    update employee controller test

--- src/main/java/br/dev/kuhn/springboottesting/controller/EmployeeController.java
+++ src/main/java/br/dev/kuhn/springboottesting/controller/EmployeeController.java
@@ -18,20 +18,30 @@ public class EmployeeController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Employee createEmployee(@RequestBody Employee employee){
+    public Employee createEmployee(@RequestBody Employee employee) {
         return employeeService.saveEmployee(employee);
     }
 
     @GetMapping
-    public List<Employee> getAllEmployees(){
+    public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployee();
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long employeeId){
-        return employeeService.getEmployeeById(employeeId)
-                .map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long employeeId) {
+        return employeeService.getEmployeeById(employeeId).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable("id") Long employeeId, @RequestBody Employee employee) {
+        return employeeService.getEmployeeById(employeeId).map(savedEmployee -> {
+            savedEmployee.setFirstName(employee.getFirstName());
+            savedEmployee.setLastName(employee.getLastName());
+            savedEmployee.setEmail(employee.getEmail());
+
+            Employee updatedEmployee = employeeService.updateEmployee(savedEmployee);
+            return new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
+        }).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 }
--- src/test/java/br/dev/kuhn/springboottesting/controller/EmployeeControllerTests.java
+++ src/test/java/br/dev/kuhn/springboottesting/controller/EmployeeControllerTests.java
@@ -4,7 +4,6 @@ import br.dev.kuhn.springboottesting.model.Employee;
 import br.dev.kuhn.springboottesting.service.EmployeeServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -16,9 +15,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -37,7 +36,50 @@ public class EmployeeControllerTests {
     private ObjectMapper objectMapper;
 
     @Test
-    public void givenInvalidEmployeeId_whenGetEmployeeById_thenReturnEmpty () throws Exception {
+    public void givenUpdatedEmployee_whenUpdateEmployee_thenReturn404() throws Exception {
+        //g
+        Long employeeId = 1L;
+        //Employee savedEmployee = Employee.builder().id(employeeId).firstName("Jardel").lastName("Kuhn").email("mail@mail.com").build();
+        Employee updatedEmployee = Employee.builder().id(employeeId).firstName("Jardel2").lastName("Kuhn2").email("mail2@mail.com").build();
+
+        given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.empty());
+        //given(employeeService.updateEmployee(any(Employee.class))).willAnswer((invocation) -> invocation.getArgument(0));
+
+        //w
+        ResultActions response = mockMvc.perform(put("/api/employees/{id}", employeeId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updatedEmployee)));
+
+        //t
+        response.andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    @Test
+    public void givenUpdatedEmployee_whenUpdateEmployee_thenReturnUpdatedEmployeeObject() throws Exception {
+        //g
+        Long employeeId = 1L;
+        Employee savedEmployee = Employee.builder().id(employeeId).firstName("Jardel").lastName("Kuhn").email("mail@mail.com").build();
+        Employee updatedEmployee = Employee.builder().id(employeeId).firstName("Jardel2").lastName("Kuhn2").email("mail2@mail.com").build();
+
+        given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.of(savedEmployee));
+        given(employeeService.updateEmployee(any(Employee.class))).willAnswer((invocation) -> invocation.getArgument(0));
+
+        //w
+        ResultActions response = mockMvc.perform(put("/api/employees/{id}", employeeId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updatedEmployee)));
+
+        //t
+        response.andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName", is(updatedEmployee.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(updatedEmployee.getLastName())))
+                .andExpect(jsonPath("$.email", is(updatedEmployee.getEmail())))
+                .andDo(print());
+    }
+
+    @Test
+    public void givenInvalidEmployeeId_whenGetEmployeeById_thenReturnEmpty() throws Exception {
         //g
         Long employeeId = 1L;
         given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.empty());
@@ -51,7 +93,7 @@ public class EmployeeControllerTests {
     }
 
     @Test
-    public void givenEmployeeId_whenGetEmployeeById_thenReturnEmployeeObject () throws Exception {
+    public void givenEmployeeId_whenGetEmployeeById_thenReturnEmployeeObject() throws Exception {
         //g
         Long employeeId = 1L;
         Employee employee = Employee.builder().id(employeeId).firstName("Jardel").lastName("Kuhn").email("mail@mail.com").build();
@@ -69,7 +111,7 @@ public class EmployeeControllerTests {
     }
 
     @Test
-    public void givenListOffEmployees_whenGetAllEmployees_thenReturnEmployeesList () throws Exception {
+    public void givenListOffEmployees_whenGetAllEmployees_thenReturnEmployeesList() throws Exception {
         //g
         List<Employee> employeeList = new ArrayList<>();
         employeeList.add(Employee.builder().firstName("Jardel").lastName("Kuhn").email("mail@mail.com").build());
@@ -88,8 +130,8 @@ public class EmployeeControllerTests {
     @Test
     public void givenEmployeeObject_whenCreateEmployee_thenReturnSavedEmployee() throws Exception {
         Employee employee = Employee.builder().firstName("Jardel").lastName("Kuhn").email("mail@mail.com").build();
-        given(employeeService.saveEmployee(ArgumentMatchers.any(Employee.class)))
-                .willAnswer((invocationOnMock) -> invocationOnMock.getArgument(0) );
+        given(employeeService.saveEmployee(any(Employee.class)))
+                .willAnswer((invocationOnMock) -> invocationOnMock.getArgument(0));
 
         ResultActions response = mockMvc.perform(post("/api/employees")
                 .contentType(MediaType.APPLICATION_JSON)
